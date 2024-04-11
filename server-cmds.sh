9b01783bd73b0806d6d4fda10756420b79a4d875
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-04-11 08:29:09 +0530

    Update server-cmds.sh

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-
-export IMAGE_NAME=$1
-export DOCKER_USER=$2
-export DOCKER_PWD=$3
-echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 docker-compose -f docker-compose.yaml up --detach --remove-orphans
 echo "success"
